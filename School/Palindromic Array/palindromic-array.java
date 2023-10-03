85ffc04b796eb5496dc60889b9d639645befabcd
itspreet003 <95958096+itspreet003@users.noreply.github.com>
2023-10-03 21:34:02 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindromic Array/palindromic-array.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.util.*;
+class PalindromicArray{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for(int i = 0 ;i < n; i++)
+				a[i]=sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.palinArray(a , n));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG
+{
+	public static int palinArray(int[] a, int n)
+           {
+                  //add code here.
+                  for(int i=0;i<n;i++){
+                      int sum=0;
+                      int temp,r;
+                      int f=a[i];
+                      temp=f;
+                      while(f>0){
+                           r=f%10;
+                           sum=(sum*10)+r;    
+                           f=f/10;    
+                      }
+                      if(temp!=sum){
+                          return 0;
+                      }
+                  }
+                  return 1;
+           }
+}
