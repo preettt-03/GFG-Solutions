b21d1758e3ae7be251b8b148b3595b32a6e05c2c
Preet Patel <95958096+preettt-03@users.noreply.github.com>
2024-01-21 20:56:23 +0530

    Added Solution - GfG to GitHub

---
+++ School/Average in a stream/average-in-a-stream.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.text.DecimalFormat;
+
+class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            float[] ans = new Solution().streamAvg(arr, n);
+            for (int i = 0; i < n; i++) {
+                DecimalFormat df = new DecimalFormat("#.##");
+            	String formatted1 = df.format(ans[i]);
+            	String formatted2 = String.format("%.2f", ans[i]);
+            	if(formatted1.length() >= 3 && formatted1.charAt(formatted1.length()-3) == '.')
+            		System.out.print(formatted1+" ");
+            	else
+            		System.out.print(formatted2+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    float[] streamAvg(int[] arr, int n) {
+        // code here
+        float[] aarr1=new float[n];
+        float a=0;
+        int b=1;
+        for(int i=0;i<n;i++){
+            a=a+arr[i];
+            aarr1[i]=a/b++;
+        }
+        return aarr1;
+    }
+}
