fd1610ecfdcd9c70b558a003778f419374c85524
Preet Patel <95958096+preettt-03@users.noreply.github.com>
2024-05-24 15:37:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Quick Sort/quick-sort.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low>=high){
+            return;
+        }
+        int s=low;
+        int e=high;
+        int m= s + (e-s)/2;
+        int pivot = arr[m];
+        
+        while(s<=e){
+            while(arr[s]<pivot){
+                s++;
+            }
+            while(arr[e]>pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp =arr[s];
+                arr[s]=arr[e];
+                arr[e]=temp;
+                s++;
+                e--;
+            }
+        }
+        quickSort(arr, low, e);
+        quickSort(arr, s, high);
+        
+    }
+   
+}
